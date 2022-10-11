c53030f27674c8e1145429886d27f72107998eb0
nia0003 <114376329+nia0003@users.noreply.github.com>
2022-10-10 20:57:56 -0500

    Add files via upload

---
+++ heyday.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Prompt the user to enter their name
+echo "War Eagle! What is your name?"
+
+# The read command assigns a user input to the variable name
+read name
+
+# Print the statement to the user
+echo "Hey $name!"
+echo "Welcome to the Auburn family. Wishing you a happy Hey Day!"
+
